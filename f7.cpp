ba6897955a6619d29e55c4e87715105840e41183
hetvi <hetvikaswala151@gmail.com>
2024-03-21 15:41:50 -0400

    first version of code, the pointers are initalized

---
+++ f7.cpp
@@ -0,0 +1,23 @@
+/*
+* File          : f7.cpp
+* Project       : SENG1000 Focused #7
+* Programmer    : Hetvi Kaswala
+* First Version : 2024-03-21
+* Description   : This program will create a text file and a binary file.
+*/
+
+#include<stdio.h>
+#pragma warning(disable : 4996)
+// binary data array given in f7 requirements
+
+
+int main(void) 
+{
+	//To create text file and binary file
+	FILE* pointer1;
+	FILE* pointer2
+	const unsigned short kBinaryData[] = { 26946, 24942, 31090, 25632, 29793, 8289, 28518, 8306, 28537, 33141, 39308 };
+	
+
+	return 0; //To run the program successfully.
+}
